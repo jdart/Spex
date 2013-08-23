f195fb7af05dfa1e31b271320567bbf7853aeeb2
jdart <jonathandart@gmail.com>
2013-08-23 14:58:33 -0500

    changed name to Spex

---
+++ spex.php
@@ -0,0 +1,64 @@
+<?php
+
+function spex() {
+
+	return wire('modules')->get('Spex');
+}
+
+/**
+ * Identical to $metric->includeScript();
+ */
+function includeScripts() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
+
+function setLayout() {
+	
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
+
+function partial() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());	
+}
+
+function addTemplateVar() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
+
+function setProductionMode() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
+
+function includeHeadScripts() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
+
+function addScript() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
+
+function addStyle() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());	
+}
+
+function includeStyles() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());	
+}
+
+function includeDocReady() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
+
+function docReady() {
+
+	return call_user_func_array(array(spex(), __METHOD__), func_get_args());
+}
