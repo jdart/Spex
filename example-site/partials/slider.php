3a4868261bfdbd0e9ec2b51135e5c4b4df5a6405
jdart <jonathandart@gmail.com>
2014-04-23 11:11:06 -0500

    added example partial

---
+++ example-site/partials/slider.php
@@ -0,0 +1,13 @@
+<?php
+
+// These are imaginary resources to demonstrate how to add js/css for this unit of code.
+$spex->addScript('libs/slider/slider.js');
+$spex->addScript('libs/slider/slider.css');
+
+if ($parent) {
+	// the $parent variable was passed in by the basic-page template.
+}
+
+?>
+
+<div>Slider here</div>
