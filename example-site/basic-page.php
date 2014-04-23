772ae2eefdcd56cede154aea64285e8fa449666f
jdart <jonathandart@gmail.com>
2014-04-23 11:29:44 -0500

    explained partial use in example-site

--- example-site/basic-page.php
+++ example-site/basic-page.php
@@ -17,6 +17,7 @@ $spex->setLayout('two-column');
 <?php // The below slot will be used later in layouts/two-column.php ?>
 <?php $spex->slot('sidebar_content', '<p>Some markup</p>') ?>
 
+<?php // Include a reusable slider with the below ?>
 <?php echo $spex->partial('slider', array('parent' => $page)) ?>
 
 <?php echo $page->body ?>
